a180f16a1ccf1d024957ed85916e3ebd7ae4fe4a
Sundaram Singh <sundaramsinghsdnr@gmail.com>
2024-04-06 10:20:19 +0530

    commit_ci3

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -6,6 +6,9 @@ import { redirect } from 'next/navigation'
 export default async function () {
   const session = await getServerSession(authOptions)
 
+
+
+  
   if(session?.user) {
     redirect('/dashboard')
   }
